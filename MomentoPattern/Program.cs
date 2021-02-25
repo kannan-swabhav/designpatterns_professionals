4709f33eb573ef011abdb9d37a3e16e87eec307a
kannan <gwa.kannan@gmail.com>
2021-02-25 11:40:58 +0530

    momento

---
+++ MomentoPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MomentoPattern
+{
+    class Program
+    {
+        //Good for doing undo mechanism
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
